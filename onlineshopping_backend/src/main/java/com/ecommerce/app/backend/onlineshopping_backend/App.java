683e83ebf29977924894169f1da0762b05a09d89
neha <neha@neha-PC>
2017-07-26 13:44:17 +0530

    Ecommerce Frontend project setup is done!

---
+++ onlineshopping_backend/src/main/java/com/ecommerce/app/backend/onlineshopping_backend/App.java
@@ -0,0 +1,13 @@
+package com.ecommerce.app.backend.onlineshopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
